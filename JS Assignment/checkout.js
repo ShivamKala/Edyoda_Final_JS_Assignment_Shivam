55a2d9f7032053e178b14bb03bc7e56a5594d89b
ShivamKala <64678339+ShivamKala@users.noreply.github.com>
2023-11-01 16:53:21 +0530

    Update checkout.js

--- JS Assignment/checkout.js
+++ JS Assignment/checkout.js
@@ -1,9 +1,16 @@
 var cart = JSON.parse(localStorage.getItem("cart")) || [];
 var itemCounts = JSON.parse(localStorage.getItem("itemCounts")) || {};
+var NavCount = localStorage.getItem("cartCount");
 
 console.log(cart);
 console.log(itemCounts);
+if (NavCount == 0 || NavCount == null || NavCount == undefined) {
+  document.getElementById("cart-count").innerText = 0;
+} else {
+  document.getElementById("cart-count").innerText = NavCount;
+}
 
+var amount = 0;
 for (var x of cart) {
   var card = document.createElement("div");
   var cardList = document.getElementById("card-list");
@@ -13,9 +20,15 @@ for (var x of cart) {
                         </div>
                         <div>
                             <h4>${x.name}</h4>
-                            <p>x3</p>
-                            <p><span>Amount: Rs </span><span>${x.price}</span></p>
+                            <p>${itemCounts[x.id] || 0}</p>
+                            <p><span id = "amount">Amount: Rs </span><span>${
+                              x.price * (itemCounts[x.id] || 0)
+                            } </span></p>
                         </div>
                     </div>`;
   cardList.append(card);
+  amount = amount + x.price * (itemCounts[x.id] || 0);
+
+  document.getElementById("total-amount").innerText = amount;
+  document.getElementById("item-count").innerText = cart.length;
 }
